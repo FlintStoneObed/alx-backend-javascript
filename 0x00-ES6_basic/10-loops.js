d3c8d1fb32f45217485e68d488d378f303aad8c7
flintstoneobed <okquartey@st.ug.edu.gh>
2024-07-22 22:22:29 +0000

    ES6_basic files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+
+  return arrayEnd;
+}
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+export default function returnHowManyArguments(...theArgs) {
+  return theArgs.length;
+}
