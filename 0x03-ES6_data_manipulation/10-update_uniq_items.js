3aae19a5bcf44b3becd26aa5aaacdb54a01447a3
flintstoneobed <okquartey@st.ug.edu.gh>
2024-07-30 07:24:35 +0000

    updated

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  return new Map(
-    Array.from(map.entries()).map(([name, quantity]) =>
-      quantity === 1 ? [name, 100] : [name, quantity]
-    )
-  );
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
 }
