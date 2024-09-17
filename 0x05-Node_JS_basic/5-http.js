fe8b72439cdd0fab77b4ef32e895400aebbaaac9
flintstoneobed <okquartey@st.ug.edu.gh>
2024-09-17 14:24:07 +0000

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,36 +1,30 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const PORT = 1245;
-
 const app = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!\n');
-    } else if (req.url === '/students') {
-      const dbPath = 'database.csv'; // You can set this path as needed
-      countStudents(dbPath)
-        .then(() => {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end();
-        })
-        .catch((error) => {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end(error.message);
-        });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found\n');
-    }
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.write(error.message);
+        res.end();
+      });
   } else {
-    res.writeHead(405, { 'Content-Type': 'text/plain' });
-    res.end('Method Not Allowed\n');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(1245);
 
 module.exports = app;
